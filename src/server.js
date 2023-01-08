473f038ff568bbfdb222327bf99081032416e4aa
Samuel Ivan <maztersamzz@gmail.com>
2023-01-08 11:22:27 +0700

    Membuat HTTP Server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
-// eslint-disable-next-line no-console
-console.log('Hallo kita akan membuat RESTful API');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
